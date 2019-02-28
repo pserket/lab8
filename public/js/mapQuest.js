3b475b7d71013197456e7761f387144d8af077ed
Pete Sheurpukdi <psheurpu@ucsd.edu>
2019-02-28 09:45:16 -0800

    thirdparty API lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,16 @@
 function initMap() {
 	// add your code here
+
+	L.mapquest.key = 'PkYANmdYcIHnE5a5XStzbJesgdyd3PZx';
+
+	var map = L.mapquest.map('map', {
+		center: [32.8788, -117.2359],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 14,
+		zoomControl: false
+	});
+
+
+
+	L.marker([32.88, -117.237]).addTo(map);
 }
